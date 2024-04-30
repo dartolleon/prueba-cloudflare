916eaa4c7e70c578d8cb8733469d01f8f812852f
dartolleon <168322333+dartolleon@users.noreply.github.com>
2024-04-30 23:00:32 +0000

    prueba imagen grande home

--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,10 @@ export const runtime = "edge";
 export default function Home() {
   return(
     <div className="flex justify-center flex-col items-center pt-10">
-      <Image src="https://images.ecestaticos.com/XDdD4yQ2fr5L-QTY2uqNp80-zlI=/0x0:2272x1514/1200x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2Fbc9%2F012%2F94b%2Fbc901294bb01bc3395dc54cf0271a316.jpg" 
+      <Image src="https://i.ibb.co/phyFYnm/nueva-modificada.webp" 
       alt="imageCaballo"
-      width={800}
-      height={800}
+      width={4096}
+      height={4096}
       />
       <h1>HOMEMEMEMEMEMEMEME</h1>
     </div>
